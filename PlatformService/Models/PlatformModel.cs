bf1d85540859e0bad312ea7ab08118e8cbe4df14
John Puddifoot <john@eastpoint.co.uk>
2021-09-26 07:54:06 +0100

    Platform repository added

--- PlatformService/Models/PlatformModel.cs
+++ PlatformService/Models/PlatformModel.cs
@@ -2,7 +2,7 @@ using System.ComponentModel.DataAnnotations;
 
 namespace PlatformService.Models
 {
-    public class Platform
+    public class PlatformModel
     {
         [Key]
         [Required]
