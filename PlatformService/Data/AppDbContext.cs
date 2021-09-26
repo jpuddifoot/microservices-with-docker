a0beaf8c0d833fa04f5a4054f4c0d71004eabb26
John Puddifoot <john@eastpoint.co.uk>
2021-09-26 08:11:05 +0100

    Correct name of Model in db context

--- PlatformService/Data/AppDbContext.cs
+++ PlatformService/Data/AppDbContext.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using PlatformService.Models;
 
 namespace PlatformService.Data
 {
@@ -9,6 +10,6 @@ namespace PlatformService.Data
             
         } 
 
-        public DbSet<Platform> Platforms {get;set;}
+        public DbSet<PlatformModel> Platforms {get;set;}
     }
 }
